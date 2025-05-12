4c3c9b3e0ee2a792f365c7bf310478a7b888a6b4
Sombian <sombian.software@gmail.com>
2025-05-12 23:46:02 +0900

    feat: enhance cmakelists.txt

--- scripts/main.cpp
+++ scripts/main.cpp
@@ -1,6 +1,13 @@
+#include <filesystem>
+
 #include "./tools/unicode.hpp"
 
 auto main() -> int
 {
+	//------------------[adjust CWD]------------------//
+	const auto CWD {std::filesystem::current_path()}; //
+	std::filesystem::current_path(CWD.parent_path()); //
+	//------------------------------------------------//
+
 	setup::unicode();
 }
